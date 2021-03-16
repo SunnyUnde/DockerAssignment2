c964025c07868bbf484b0b359d36e72d359c36a3
SunnyUnde <65486900+SunnyUnde@users.noreply.github.com>
2021-03-16 22:38:35 +0530

    Add files via upload
    SQL file containing the required SQL data for the assignment.

---
+++ schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS studentData;
+
+CREATE TABLE  studentData
+(
+Name char(25),
+Roll_no int,
+Address varchar(40),
+Mobile varchar(10),
+PAN_Number varchar(10)
+);
+
+INSERT INTO studentData VALUES('Sunny','1043','Deolali Pravara','123456789','ABCPU123A');
+INSERT INTO studentData VALUES('Suresh','1023','Sangamner','123654789','ABDPA122B');
+INSERT INTO studentData VALUES('Ramesh','1045','Akola','178556789','ABEPV113F');
+INSERT INTO studentData VALUES('Kavita','1013','Ahmednagar','123452569','ABFPK127K');
+INSERT INTO studentData VALUES('Sangita','2032','Pune','123743789','ABGPL123L');
+INSERT INTO studentData VALUES('Surya','2028','Ratnagiri','984456789','ABHPP186A');
+INSERT INTO studentData VALUES('Luffy','2001','Kota','123267789','ABIPY172B');
+INSERT INTO studentData VALUES('Light','1021','Bhopal','123456654','ABKPA158Q');
+INSERT INTO studentData VALUES('Kiran','2034','Mysore','123695789','ABLPN198N');
